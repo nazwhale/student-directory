cc94ac1d0f4ffd2f4277dcbca0a9682370ae70e9
Naz <nazmalik08@gmail.com>
2017-02-28 14:38:03 +0000

    extra features from ex8 removed

--- directory.rb
+++ directory.rb
@@ -3,38 +3,16 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #get first name
+  # get the first name
   name = gets.chomp
-  cohorts = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  #while name is not empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
-    cohort = enter_cohort
-    until cohorts.include? cohort
-      puts "Value entered is not a month"
-      cohort = enter_cohort
-    end
-    cohort = cohort.to_s
-    @students << {name: name, cohort: cohort}
-    if @students.count == 1
-      puts "Now we have 1 student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    #get another name from the user
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
   end
-end
-
-def enter_cohort
-  puts "Enter cohort month"
-  puts "To use default value, just hit return"
-  cohort = gets.chomp
-  cohort = cohort.capitalize
-  if cohort == ""
-    cohort = "November"
-    puts "No month entered. Cohort set to default month (November)"
-  end
-  cohort
 end
 
 def interactive_menu
@@ -76,10 +54,8 @@ def print_header
 end
 
 def print_students_list
-  count = 0
-  until count == @students.length
-    puts "#{count+1}. #{@students[count][:name]} (#{@students[count][:cohort]} cohort)"
-    count +=1
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
