5918e5c63f4752d85536d5ff853918cdf9c227c0
Naz <nazmalik08@gmail.com>
2017-02-28 13:13:56 +0000

    ex9: interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+    #1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    #2. read the input and save it into a variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -102,15 +127,4 @@ def print_by_cohorts(students)
     students.each {|student| puts student[:name] if student[:cohort] == cohort}
   end
 end
-
-
-students = input_students
-#nothing happens until we call the methods
-print_header
-if students.count == 0
-  puts "There are currently no students at Villains Academy"
-else
-  print(students)
-  print_by_cohorts(students)
-  print_footer(students)
-end
+interactive_menu
