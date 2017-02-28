d1fd30021610aceee79c8aab48608a871b6c91d5
Naz <nazmalik08@gmail.com>
2017-02-28 15:06:50 +0000

    ex11: save students option added, .gitignore file added

--- directory.rb
+++ directory.rb
@@ -1,16 +1,28 @@
 @students = []
 
+def save_students
+  #open file for writing
+  file = File.open("students.csv", "w")
+  #iterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
+  #get first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  #while name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
+    #add student hash to array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    # get another name from the user
+    #get another name from the user
     name = gets.chomp
   end
 end
@@ -25,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -40,6 +53,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -66,6 +81,5 @@ def print_footer
     puts "Overall, we have #{@students.count} great students"
   end
 end
-
 
 interactive_menu
