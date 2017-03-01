df3314af89e91ad11113575f04111c9ce1a8bb68
Naz <nazmalik08@gmail.com>
2017-03-01 13:05:39 +0000

    ex14.6: files set to open/close with block

--- directory.rb
+++ directory.rb
@@ -91,22 +91,22 @@ def print_footer
 end
 
 def save_students(filename="students.csv")
-  file = File.open(filename, "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def load_students(filename="students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    add_students(name, cohort)
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(",")
+      add_students(name, cohort)
+    end
   end
-  file.close
 end
 
 def add_students(name, cohort=:november)
